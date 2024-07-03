b26ae6e9c47e4e662923ae60ee68f1d81fad4790
Rafiq <rafiqshaik097@gmail.com>
2024-07-03 07:37:02 +0530

    improvement

--- utils/check.js
+++ utils/check.js
@@ -5,7 +5,7 @@ function checkTime()
 {
     const now = new Date();
     console.log(now);
-    if(now.getHours() === 7 && now.getMinutes() === 30){
+    if(now.getHours() === 7 && (now.getMinutes() === 45 || now.getMinutes() === 50 || now.getMinutes() === 55) ){
         sendCronEmails();
         sendEmailStats();
     }
